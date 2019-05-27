8c4dfb99f48c6625e832d474ed1bd99df1150067
Vladimir <woyaxx@gmail.com>
2019-05-28 01:30:23 +0300

    Add files via upload

---
+++ Monticello/js/script.js
@@ -0,0 +1,13 @@
+;(function($){
+    "use strict";
+
+$(function(){
+    $('.ba-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+})
+
+
+})(jQuery);
