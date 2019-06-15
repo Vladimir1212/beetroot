7aebfba56e135a6ae41cc6729083a7f29702b0f1
Vladimir <woyaxx@gmail.com>
2019-06-15 19:52:53 +0300

    Add files via upload

--- Portfolio/build/script.js
+++ Portfolio/build/script.js
@@ -1,3 +1,53 @@
+$(function() {
+  $(".menu").click(function() {
+    if ($("#navigation").hasClass("hidden")) {
+      $("#navigation").attr("class", "visible animated slideInLeft");
+    } else {
+      $("#navigation").attr("class", "hidden animated slideOutLeft");
+    }
+    $(this).toggleClass("open");
+  });
+
+  $("#navigation").click(function() {
+    if ($("#navigation").hasClass("visible")) {
+      $(".menu").toggleClass("open");
+    } else {
+    }
+    $(this).attr("class", "slideOutLeft hidden");
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      if ($("#navigation").hasClass("visible")) {
+        $(".menu").toggleClass("open");
+      } else {
+      }
+      $("#navigation").attr("class", "slideOutLeft hidden");
+    }
+  });
+});
+
+
+
+
+$(document).ready(function(){
+    $("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#pagination").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
 $(document).ready(function() {
 
     $('#accordeon .acc_head').on('click', f_acc);
@@ -9,3 +59,4 @@ function f_acc(){
 
     $(this).next().slideToggle(1000);
 }
+
